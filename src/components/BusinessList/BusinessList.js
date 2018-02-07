67c2562605be80e4d680351e093e279ca60acb75
Zhou Ziyu <ziyu_zhou_victoria@163.com>
2018-02-07 13:00:28 +0800

    Manually deleted all merging conflicts info

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
@@ -16,22 +15,3 @@ class BusinessList extends React.Component {
 }
 
 export default BusinessList;
-=======
-import React from 'react';
-import './BusinessList.css';
-import Business from '../Business/Business';
-
-class BusinessList extends React.Component {
-  render() {
-    return(
-      <div className="BusinessList">
-        {this.props.businesses.map(business => {
-            return <Business key={business.id} business={business} />})
-        }
-      </div>
-    );
-  }
-}
-
-export default BusinessList;
->>>>>>> 105d7e4f79037450e585cbaa5ca355aad98bc30e
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const apiKey = 'n5Ube3eczbax-5r8rMDAGvxcUTgnZQl96edwEqE2X09D6OM2haMUnMY2CJ5U3MjnzD_Nndb9u877dFd4YvJSGhUKXGmYr6KhNvZLu9eWA4BifsTUzsxO1cE3hJR5WnYx';
 
 let Yelp = {
@@ -31,37 +30,3 @@ let Yelp = {
   }
 };
  export default Yelp;
-=======
-const apiKey = 'n5Ube3eczbax-5r8rMDAGvxcUTgnZQl96edwEqE2X09D6OM2haMUnMY2CJ5U3MjnzD_Nndb9u877dFd4YvJSGhUKXGmYr6KhNvZLu9eWA4BifsTUzsxO1cE3hJR5WnYx';
-
-let Yelp = {
-  search: function(term, location, sortBy) {
-    return fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term='+ term +
-    '&location=' + location + '&sort_by=' + sortBy, {
-  headers: {
-     Authorization: `Bearer ${apiKey}`
-  }
-}).then(response => {return response.json();
-}).then(jsonResponse => {
-  if(jsonResponse.business) {
-    return jsonResponse.businesses.map(business =>  ({
-      id: business.id,
-      imageSrc: business.image_url,
-      name: business.name,
-      address: business.location.address1,
-      city: business.location.city,
-      state: business.location.state,
-      zipCode: business.location.zip_code,
-      category: business.categories[0].title,
-      rating: business.rating,
-      reviewCount: business.review_count
-    }));
-  } else {
-    console.log(jsonResponse);
-    return [];
-  }
-});
-  }
-};
- export default Yelp;
->>>>>>> 105d7e4f79037450e585cbaa5ca355aad98bc30e
